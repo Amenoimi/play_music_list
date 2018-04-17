bd14d0b02cd544121a8ffecd5a17f9df9d09fe11
Amenoimi <bmpss92285@gmail.com>
2018-04-17 12:43:40 +0800

    主要檔案

---
+++ 音樂歌單播放/音樂歌單播放/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(15, 65)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(289, 220)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(22, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(72, 38)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "播放"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(22, 113)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(72, 38)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "暫停"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(310, 162)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(29, 38)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "刪除"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(15, 21)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(72, 38)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "隨機撥放"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button7)
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(354, 310)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "檔案"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 288)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 12)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "請將要聽歌曲拖曳至方框內"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(310, 218)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(29, 32)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "向下"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(310, 108)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(29, 32)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "向上"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(232, 21)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(72, 38)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "一般播放"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Location = New System.Drawing.Point(387, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(112, 171)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "撥放"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button10)
+        Me.GroupBox3.Controls.Add(Me.Button9)
+        Me.GroupBox3.Controls.Add(Me.Button8)
+        Me.GroupBox3.Location = New System.Drawing.Point(387, 201)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(110, 121)
+        Me.GroupBox3.TabIndex = 7
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "歌單"
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(6, 79)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(98, 23)
+        Me.Button10.TabIndex = 2
+        Me.Button10.Text = "清空歌單"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(6, 50)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(98, 23)
+        Me.Button9.TabIndex = 1
+        Me.Button9.Text = "讀取上次歌單"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(6, 21)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(98, 23)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = "寫入目前歌單"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(511, 338)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "音樂歌單撥放"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+End Class
---
+++ 音樂歌單播放/音樂歌單播放/Form1.vb
@@ -0,0 +1,338 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Public Const MM_MCINOTIFY As Integer = 953
+    Dim adio As New List(Of String)
+    Dim palyimg_mod As Integer
+
+    ' 宣告 API 
+    Private Declare Ansi Function mciSendStringA Lib "winmm.dll" Alias "mciSendStringA" (ByVal command As String, ByRef buffer As StringBuilder, ByVal bufferSize As Integer, ByVal hWndCallback As IntPtr) As Integer
+
+
+    Private Declare Auto Function GetShortPathName Lib "kernel32" (
+        ByVal longPath As String,
+        ByVal shortPath As StringBuilder,
+        ByVal shortBufferSize As Int32) As Int32
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = MM_MCINOTIFY Then
+            ' The file is done playing, do whatever is necessary at this point
+            'MsgBox("Done playing.")
+            If (Button1.Text = "停止" And Button2.Text = "暫停") Then
+                If (ListBox1.SelectedIndex < ListBox1.Items.Count - 1) Then
+                    If (palyimg_mod = 0) Then
+                        ListBox1.SelectedIndex = ListBox1.SelectedIndex + 1
+                        PlayMediaFile(adio(ListBox1.SelectedIndex))
+                    Else
+                        Dim rnd As New Random
+                        Dim s, d As Integer
+                        d = ListBox1.Items.Count - 1
+                        s = rnd.Next(0, d)
+                        ListBox1.SelectedIndex = s
+                        PlayMediaFile(adio(ListBox1.SelectedIndex))
+                    End If
+                ElseIf (ListBox1.SelectedIndex = ListBox1.Items.Count - 1) Then
+                    Button1.Text = "播放"
+                    Button2.Text = "暫停"
+                End If
+
+
+
+            End If
+
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    '播放媒體檔案
+    Private Function PlayMediaFile(ByVal mediaFile As String) As Boolean
+        If File.Exists(mediaFile) Then
+            mediaFile = ConvertToShortPathName(mediaFile)
+            mciSendStringA("stop music", Nothing, 0, 0)
+            mciSendStringA("close music", Nothing, 0, 0)
+            mciSendStringA(String.Format("open {0} alias music", mediaFile), Nothing, 0, 0)
+            PlayMediaFile = mciSendStringA("play music notify", Nothing, 0, Me.Handle.ToInt64()) = 0
+        End If
+    End Function
+
+
+    '取得短路徑及檔名
+    Private Function ConvertToShortPathName(fileName As String) As String
+        Dim shortName As New StringBuilder(256)
+        GetShortPathName(fileName, shortName, shortName.Capacity)
+        Return shortName.ToString
+    End Function
+
+    ' 停止播放
+    Private Function StopMedia() As Boolean
+        StopMedia = mciSendStringA("stop music", Nothing, 0, 0) = 0
+        mciSendStringA("close music", Nothing, 0, 0)
+    End Function
+
+    ' 暫停播放
+    Private Function PauseMedia() As Boolean
+        Return mciSendStringA("pause music", Nothing, 0, 0) = 0
+    End Function
+
+    ' 繼續播放
+    Private Function ContinueMedia() As Boolean
+        Return mciSendStringA("play music", Nothing, 0, 0) = 0
+    End Function
+
+
+    Private Sub ListBox1_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox1.DragEnter
+        'e.Effect = DragDropEffects.All
+
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox1.DragDrop
+
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+
+        For Each path In files
+            'ReDim Preserve adio(i + 1)
+            'adio(i - 1) = path.ToString
+            Dim data As String() = path.Split("."c)
+            If (data(data.Length - 1) = "wav" Or data(data.Length - 1) = "mp3" Or data(data.Length - 1) = "mid") Then
+                adio.Add(path)
+            End If
+
+
+        Next
+        ListBox1.Items.Clear()
+
+        For Each path In adio
+            Dim name As String() = path.Split("\"c)
+            ListBox1.Items.Add(name(name.Length - 1))
+
+        Next
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+        palyimg_mod = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (Button1.Text = "播放" And ListBox1.SelectedIndex <> -1) Then
+
+            PlayMediaFile(adio(ListBox1.SelectedIndex))
+
+            Button1.Text = "停止"
+
+        ElseIf (Button1.Text = "播放" And ListBox1.SelectedIndex = -1 And ListBox1.Items.Count > 0) Then
+            ListBox1.SelectedIndex = 0
+            PlayMediaFile(adio(ListBox1.SelectedIndex))
+            Button1.Text = "停止"
+
+        ElseIf (Button1.Text = "停止") Then
+            StopMedia()
+            Button1.Text = "播放"
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (palyimg_mod = 0) Then
+            Button5.Enabled = False
+            Button4.Enabled = True
+
+        Else
+            Button5.Enabled = True
+            Button4.Enabled = False
+        End If
+        For Each f In adio
+            If (f = "") Then
+                ccc()
+            End If
+
+        Next
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (Button2.Text = "繼續" And ListBox1.SelectedIndex <> -1) Then
+            ContinueMedia()
+            Button2.Text = "暫停"
+        ElseIf (Button2.Text = "暫停") Then
+            PauseMedia()
+            Button2.Text = "繼續"
+        End If
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (ListBox1.SelectedIndex <> -1) Then
+            Dim tmp_adio As New List(Of String)
+            For Each no_del In adio
+                If (no_del <> adio(ListBox1.SelectedIndex)) Then
+                    tmp_adio.Add(no_del)
+                End If
+
+            Next
+            adio = tmp_adio
+            ListBox1.Items.Clear()
+
+            For Each path In adio
+                Dim name As String() = path.Split("\"c)
+                ListBox1.Items.Add(name(name.Length - 1))
+
+            Next
+
+        End If
+    End Sub
+
+    Sub ccc()
+        Dim tmp_adio As New List(Of String)
+        For Each no_del In adio
+            If (no_del <> "") Then
+                tmp_adio.Add(no_del)
+            End If
+
+        Next
+        adio = tmp_adio
+        ListBox1.Items.Clear()
+
+        For Each path In adio
+            Dim name As String() = path.Split("\"c)
+            ListBox1.Items.Add(name(name.Length - 1))
+
+        Next
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        palyimg_mod = 0
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        palyimg_mod = 1
+    End Sub
+
+    Sub w_txt(strTemp As String)
+        Dim FileNum As Integer
+
+
+        FileNum = FreeFile()
+        FileOpen(FileNum, Application.StartupPath + "\tmp.txt", OpenMode.Output)
+
+
+        PrintLine(FileNum, strTemp)
+
+        FileClose(FileNum)
+    End Sub
+    Function r_txt()
+        Dim FileNum As Integer
+        Dim strTemp As String
+
+        FileNum = FreeFile()
+        FileOpen(FileNum, Application.StartupPath + "\tmp.txt", OpenMode.Input)
+
+        Do Until EOF(FileNum)
+            strTemp &= LineInput(FileNum) & vbNewLine
+        Loop
+
+        FileClose(FileNum)
+        Return strTemp
+    End Function
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim txt As String
+        For Each a In adio
+            txt += a + "###"
+        Next
+
+        w_txt(txt)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+        adio = New List(Of String)
+        ListBox1.Items.Clear()
+
+        Dim ts As String
+        Dim tss As String()
+
+        ts = r_txt()
+        tss = ts.Split("###")
+
+
+        For Each path In tss
+            'ReDim Preserve adio(i + 1)
+            'adio(i - 1) = path.ToString
+            adio.Add(path)
+
+        Next
+        ListBox1.Items.Clear()
+
+        For Each path In adio
+            Dim name As String() = path.Split("\"c)
+            ListBox1.Items.Add(name(name.Length - 1))
+
+        Next
+
+
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        adio = New List(Of String)
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (ListBox1.SelectedIndex <> -1 And (ListBox1.SelectedIndex <> 0)) Then
+            Dim g, g2 As String
+            Dim p As Integer
+            p = ListBox1.SelectedIndex
+            g = ListBox1.Items(ListBox1.SelectedIndex)
+            g2 = adio(ListBox1.SelectedIndex)
+            ListBox1.Items(ListBox1.SelectedIndex) = ListBox1.Items(ListBox1.SelectedIndex - 1)
+            adio(ListBox1.SelectedIndex) = adio(ListBox1.SelectedIndex - 1)
+            ListBox1.Items(ListBox1.SelectedIndex - 1) = g
+            adio(ListBox1.SelectedIndex - 1) = g2
+
+            ListBox1.Items.Clear()
+
+            For Each path In adio
+                Dim name As String() = path.Split("\"c)
+                ListBox1.Items.Add(name(name.Length - 1))
+
+            Next
+            ListBox1.SelectedIndex = p - 1
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If (ListBox1.SelectedIndex <> -1 And ListBox1.SelectedIndex <> (ListBox1.Items.Count - 1)) Then
+            Dim g, g2 As String
+            Dim p As Integer
+            p = ListBox1.SelectedIndex
+            g = ListBox1.Items(ListBox1.SelectedIndex)
+            g2 = adio(ListBox1.SelectedIndex)
+            ListBox1.Items(ListBox1.SelectedIndex) = ListBox1.Items(ListBox1.SelectedIndex + 1)
+            adio(ListBox1.SelectedIndex) = adio(ListBox1.SelectedIndex + 1)
+            ListBox1.Items(ListBox1.SelectedIndex + 1) = g
+            adio(ListBox1.SelectedIndex + 1) = g2
+
+            ListBox1.Items.Clear()
+
+            For Each path In adio
+                Dim name As String() = path.Split("\"c)
+                ListBox1.Items.Add(name(name.Length - 1))
+
+            Next
+            ListBox1.SelectedIndex = p + 1
+        End If
+    End Sub
+End Class
